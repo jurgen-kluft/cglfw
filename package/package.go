60cb4da5658f6788b059a44a91b7975278d23989
Jurgen Kluft <jurgen@virtuosgames.com>
2022-02-18 14:14:56 +0800

    * first commit

---
+++ package/package.go
@@ -0,0 +1,37 @@
+package libglfw
+
+import (
+	"github.com/jurgen-kluft/xbase/package"
+	"github.com/jurgen-kluft/xcode/denv"
+	"github.com/jurgen-kluft/xentry/package"
+	"github.com/jurgen-kluft/xunittest/package"
+)
+
+// GetPackage returns the package object of 'libglfw'
+func GetPackage() *denv.Package {
+	// Dependencies
+	xunittestpkg := xunittest.GetPackage()
+	xentrypkg := xentry.GetPackage()
+	xbasepkg := xbase.GetPackage()
+
+	// The main (libglfw) package
+	mainpkg := denv.NewPackage("libglfw")
+	mainpkg.AddPackage(xunittestpkg)
+	mainpkg.AddPackage(xentrypkg)
+	mainpkg.AddPackage(xbasepkg)
+
+	// 'libglfw' library
+	mainlib := denv.SetupDefaultCppLibProject("libglfw", "github.com\\jurgen-kluft\\libglfw")
+	mainlib.Dependencies = append(mainlib.Dependencies, xbasepkg.GetMainLib())
+
+	// 'libglfw' unittest project
+	maintest := denv.SetupDefaultCppTestProject("libglfw_test", "github.com\\jurgen-kluft\\libglfw")
+	maintest.Dependencies = append(maintest.Dependencies, xunittestpkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, xentrypkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, xbasepkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, mainlib)
+
+	mainpkg.AddMainLib(mainlib)
+	mainpkg.AddUnittest(maintest)
+	return mainpkg
+}
---
+++ source/test/cpp/test_basics.cpp
@@ -0,0 +1,21 @@
+#include "xbase/x_target.h"
+#include "xbase/x_buffer.h"
+
+#include "xunittest/xunittest.h"
+
+using namespace xcore;
+
+UNITTEST_SUITE_BEGIN(basics)
+{
+	UNITTEST_FIXTURE(basics)
+	{
+		UNITTEST_FIXTURE_SETUP() {}
+		UNITTEST_FIXTURE_TEARDOWN() {}
+
+		UNITTEST_TEST(test)
+		{
+		}
+		
+	}
+}
+UNITTEST_SUITE_END
