91d813d933b9044ce8dda212a968a02236d1bbb1
Jurgen <jurgen.kluft@gmail.com>
2022-02-19 20:50:22 +0800

    * correctly generating tundra build files

--- package/package.go
+++ package/package.go
@@ -16,12 +16,12 @@ func GetPackage() *denv.Package {
 
 	// The main (libglfw) package
 	mainpkg := denv.NewPackage("libglfw")
-	mainpkg.AddPackage(xunittestpkg)
 	mainpkg.AddPackage(xentrypkg)
 	mainpkg.AddPackage(xbasepkg)
 
 	// 'libglfw' library
 	mainlib := denv.SetupDefaultCppLibProject("libglfw", "github.com\\jurgen-kluft\\libglfw")
+	mainlib.AddDefaultCocoa()
 	mainlib.Dependencies = append(mainlib.Dependencies, xbasepkg.GetMainLib())
 
 	// 'libglfw' unittest project
@@ -33,13 +33,23 @@ func GetPackage() *denv.Package {
 
 	if denv.XCodeOS == "windows" {
 		mainlib.AddDefine("_GLFW_WIN32")
+		mainlib.AddDefine("_GLFW_WGL")
+		mainlib.AddDefine("WIN32")
 		maintest.AddDefine("_GLFW_WIN32")
+		maintest.AddDefine("_GLFW_WGL")
+		maintest.AddDefine("WIN32")
 	} else if denv.XCodeOS == "darwin" {
 		mainlib.AddDefine("_GLFW_COCOA")
+		mainlib.AddDefine("MACOSX")
 		maintest.AddDefine("_GLFW_COCOA")
+		maintest.AddDefine("MACOSX")
 	} else if denv.XCodeOS == "linux" {
 		mainlib.AddDefine("_GLFW_X11")
+		mainlib.AddDefine("_GLFW_GFX")
+		mainlib.AddDefine("LINUX")
 		maintest.AddDefine("_GLFW_X11")
+		maintest.AddDefine("_GLFW_GFX")
+		maintest.AddDefine("LINUX")
 	}
 
 	mainpkg.AddMainLib(mainlib)
